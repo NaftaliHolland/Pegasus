577d3c484852e082263db9c3e190e86a293156cc
MosesMuiru <muirumoses9@gmail.com>
2024-05-07 14:15:40 +0300

    implementng stk push

--- payment/lib/payment/gateway/make_payment.ex
+++ payment/lib/payment/gateway/make_payment.ex
@@ -1,29 +1,48 @@
 defmodule Payment.Gateway.MakePayment do
   alias Payment.Gateway.Auth
-    
+
   use Tesla
-  
 
   plug Tesla.Middleware.BaseUrl, "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
-  
 
   @spec password :: String.t()
   def password do
-    Base.encode64(encode_data)
-    
+    Base.encode64(encode_data())
   end
 
-  @spec encode_date :: String.t()
+  @spec encode_data :: String.t()
   def encode_data do
     %{"access_token" => access_token} = Auth.get_auth_key()
-    "174379#{DateTime.utc_now() |> DateTime.to_string()}#{access_token}"
-
+    "174379#{date_format()}#{access_token}"
   end
 
-  @spec pay(String.t(), integer) :: any()
-  def pay(short_code, amount) do
-    
-    
-
+  def date_format do
+    DateTime.utc_now()
+    |> DateTime.to_string()
+    |> String.replace("-", "")
+    |> String.replace(" ", "")
+    |> String.replace(":", "")
+    |> String.replace(".", "")
+    |> String.replace("Z", "")
   end
+
+  def pay(buy_for, pay_from, amount) do
+    payment_details = %{
+      "BusinessShortCode" => "174379",
+      "Password" => password(),
+      "Timestamp" => date_format(),
+      "TransactionType" => "CustomerPayBillOnline",
+      "Amount" => amount,
+      "PartyA" => buy_for,
+      "PartyB" => "174379",
+      "PhoneNumber" => pay_from,
+      "CallBackURL" => "https://mydomain.com/pat",
+      "AccountReference" => "Test",
+      "TransactionDesc" => "Test"
+    }
+
+    post("/", payment_details)
+  end
+
+  
 end
--- payment/mix.exs
+++ payment/mix.exs
@@ -58,7 +58,8 @@ defmodule Payment.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:tesla, "~> 1.9"}
+      {:tesla, "~> 1.9"},
+      {:timex, "~> 3.7"}
     ]
   end
 
