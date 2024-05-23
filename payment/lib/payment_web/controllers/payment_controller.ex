d9dc21865f4db1b09db63526b76a250d208cecb3
MosesMuiru <muirumoses9@gmail.com>
2024-05-23 03:49:03 +0300

    [fix] format of the stk push

--- payment/lib/payment_web/controllers/payment_controller.ex
+++ payment/lib/payment_web/controllers/payment_controller.ex
@@ -6,9 +6,10 @@ defmodule PaymentWeb.PaymentController do
     %{"buy_for" => buy_for, "pay_from" => pay_from, "amount" => amount} = params
     {:ok, body} = MakePayment.pay(buy_for, pay_from, amount)
 
-    IO.inspect(body)
+    {:ok, response} = Jason.encode(body.body)
+
     conn
+    |> send_resp(body.status, response)
     |> put_status(200)
-    
   end
 end
--- payment/test/gateway_test/make_payment_test.ex
+++ payment/test/gateway_test/make_payment_test.ex
@@ -1,24 +1,16 @@
 defmodule PaymentTest.MakePaymentTest do
-
   use ExUnit.Case
 
   setup do
-
-    mock(
-      fn
-        %{method: :post, url: "https://makepayment"} ->
-
-          %Tesla.Env{status: 200, body: "payment made"}
-      end
-    )
+    mock(fn
+      %{method: :post, url: "https://makepayment"} ->
+        %Tesla.Env{status: 200, body: "payment made"}
+    end)
   end
 
-
-  test do "testing making stk push " do
-
+  test "testing making stk push " do
     assert {:ok, %Tesla.Env{} = env} = post("https://makepayment")
     assert env.status == 200
     assert env.body == "payment made"
   end
-
 end
